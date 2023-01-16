4c1502c48ec7b5e028ba9ef0c9ad85e5d0079ceb
thekingwars <carlosguerra2001.2@gmail.com>
2023-01-15 20:59:43 -0400

    update enviroment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  api: 'https://pokeapi.co/api/v2/pokemon/',
+  lastPagePokemon: 'https://pokeapi.co/api/v2/pokemon/?offset=1259&limit=20',
+  firstPagePokemon: 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20',
 };
